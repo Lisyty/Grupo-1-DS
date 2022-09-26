96fb4aa8f87960a04cc48f3a64d07fc9828e93cf
YohanAlfredo <111163464+YohanAlfredo@users.noreply.github.com>
2022-09-26 06:40:49 -0500

    AppContext, base de datos

--- AplicacionEstudiantil.App.Persistencia/AppRepositorio/AppContext.cs
+++ AplicacionEstudiantil.App.Persistencia/AppRepositorio/AppContext.cs
@@ -9,7 +9,7 @@ namespace AplicacionEstudiantil.App.Persistencia{
         public DbSet<Estudiante> Estudiantes { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){
             if (!optionsBuilder.IsConfigured){
-                optionsBuilder.UseSqlServer("Data Source = (Localdb)\\MSSQLLocalDB; Initial Catalog = AplicacionEducativa_Equipo_1");//conexion a base de datos local
+                optionsBuilder.UseSqlServer("Data Source=SQL8002.site4now.net;Initial Catalog=db_a8d4e8_dijaliyo001;User Id=db_a8d4e8_dijaliyo001_admin;Password=MISIONES-tic2022"");//conexion a base de datos local
             }
         }
     }
